2d437a9f7db03d265ad7ad43cf13b9d2b710d809
ivan <ivan-guita@hotmail.com>
2024-05-26 17:37:12 -0300

    comunicação com mongo db estabelecida

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 let express = require("express");
 let app = express();
+let mongoose = require('mongoose')
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/guiapics",{useNewUrlParser:true, useUnifiedTopology:true});
 
 app.get("/",(req,res)=>{
     res.json({});
